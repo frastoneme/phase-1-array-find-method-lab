aa255ee9cf3d4ca59d658c9a14c7ffcbbf87b60a
frastoneme <oigoeme@gmail.com>
2024-03-26 17:26:39 +0300

    using find function

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"}, //Denver Broncos football results
+    { year: "2013", result: "L"},
+  ]
+  superbowlWin = (record) => {
+    const result = record.find( record => record.result === "W" );
+    return result ? result.year : undefined; // changing to truthy and falsy
+  }
+  console.log(superbowlWin(record));
